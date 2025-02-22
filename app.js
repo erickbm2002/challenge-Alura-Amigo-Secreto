2d3d9a4a9b32386fe1870f36a0f30abf33048ebe
erickbm2002 <eriickbarriientos42@gmail.com>
2025-02-21 20:12:01 -0600

    Se genera las funciones para actulizar y agragar a un amigo, adicional se cambia el nombre de la funcion en HTML

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigosArray() {
+    let amigoIngresado = document.getElementById("amigo").value
+
+    if (!amigoIngresado) {
+        return alert("Por favor ingrese un nombre valido")
+    }
+    amigos.push(amigoIngresado)
+    document.getElementById("amigo").value = "";
+    console.log(amigoIngresado)
+    actulizarListaAmigos(amigos)
+}
+
+function actulizarListaAmigos (array) {
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML = ""
+
+    for (let i = 0; i < array.length;i++) {
+        const nuevoElementoLista = document.createElement("li")
+        nuevoElementoLista.textContent = array[i]
+        lista.appendChild(nuevoElementoLista)
+    }
+
+    
+    
+}   
