5354629978639d03b1d327d15e10e83bd915b671
erickbm2002 <eriickbarriientos42@gmail.com>
2025-02-21 23:17:55 -0600

    Se agregan funcionalidades para mostrar los nombres y se genera la funcionalidad para sortear los nombres. Adicional se empieza en la contruccion de una nuvea funcionalidad para reinciar el sorteo

--- app.js
+++ app.js
@@ -2,27 +2,58 @@
 let amigos = [];
 
 function agregarAmigosArray() {
-    let amigoIngresado = document.getElementById("amigo").value
-
+    let amigoIngresado = obtenerDato("amigo").value
     if (!amigoIngresado) {
         return alert("Por favor ingrese un nombre valido")
     }
     amigos.push(amigoIngresado)
-    document.getElementById("amigo").value = "";
-    console.log(amigoIngresado)
+    limpiarDatos("amigo")
     actulizarListaAmigos(amigos)
 }
 
 function actulizarListaAmigos (array) {
-    let lista = document.getElementById("listaAmigos")
-    lista.innerHTML = ""
+    let lista = obtenerDato("listaAmigos")
+    limpiarDatos("listaAmigos")
 
     for (let i = 0; i < array.length;i++) {
         const nuevoElementoLista = document.createElement("li")
         nuevoElementoLista.textContent = array[i]
         lista.appendChild(nuevoElementoLista)
     }
-
-    
-    
 }   
+
+
+function sortearAmigo() {
+    if (!amigos.length) {
+        return alert("Debe primero ingresar nombres para obtener al amigo secreto")
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length)
+    let elementoUL = obtenerDato("resultado")
+    elementoUL.innerHTML = amigos[indiceAleatorio]
+    obtenerDato("reiniciar").removeAttribute("disabled")
+}
+
+function obtenerDato(elementoId) {
+    return document.getElementById(elementoId)
+
+}
+
+function limpiarDatos(elementoId) {
+    let elementoHTML = obtenerDato(elementoId)
+    if(elementoHTML.tagName === "INPUT") {
+        elementoHTML.value = "";
+    } else {
+        elementoHTML.innerHTML = ""
+    }
+}
+
+function reiniciarSorteo() {
+    amigos = []
+    elementoHTML = obtenerDato("listaAmigos")
+    elementoHTML.innerHTML = ""
+
+}
+
+function reiniciarSorteo() {
+    
+}
